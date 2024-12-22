97d5225b193feecc4c80d53b2b1481b4abf1ded7
NEroousl <s92083180@ousl.lk>
2024-12-22 07:26:10 +0530

    bug fix

--- download_playlist.py
+++ download_playlist.py
@@ -114,35 +114,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-def header():
-    print("""        
-         __   __         _____      _                             
-         ` ` / /__  _   |_   _|   _| |__   ___                    
-          ` V / _ `| | | || || | | | '_ ` / _ `                   
-           | | (_) | |_| || || |_| | |_) |  __/                   
-          _|_|`___/ `__,_||_| `__,_|_.__/ `___/                   
-         |  _ `| | __ _ _   _| (_)___| |_                         
-         | |_) | |/ _` | | | | | / __| __|                        
-         |  __/| | (_| | |_| | | `__ ` |_                         
-         |_|__ |_|`__,_|`__, |_|_|___/`__|            _           
-         |  _ `  _____  |___/__ __ | | ___   __ _  __| | ___ _ __ 
-         | | | |/ _ ` ` /` / / '_ `| |/ _ ` / _` |/ _` |/ _ ` '__|
-         | |_| | (_) ` V  V /| | | | | (_) | (_| | (_| |  __/ |   
-         |____/ `___/ `_/`_/ |_| |_|_|`___/ `__,_|`__,_|`___|_|   
-                                                                  
-""")
-    print("\n            -Malinda Vitharana @NEroousl-")
-    print("    Download all your favorite videos with ease!  \n\n")
-
-# Main function to orchestrate the download
-def main():
-    header()
-    url = get_playlist_url()
-    quality = choose_quality()
-    output_folder = get_output_folder()
-    download_playlist(url, quality, output_folder)
-
-if __name__ == '__main__':
-    main()
